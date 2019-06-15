23e6aeb6f137c97924bbe7bddb1a15f8606e20b5
PeterBelzman <petka751@gmail.com>
2019-06-15 15:47:28 +0300

    TODO-05: add state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './todo-list-item.css';
 
@@ -7,35 +7,47 @@ export default class TodoListItem extends Component {
 
     constructor() {
         super();
-        this.onLabelCkick = () => {
-            console.log(`Done ${this.props.label}`)
-        };
-    }
 
-    render () {
-        const { label, important = false} = this.props; // передача свойств - свойства хранятся в this.props
+        this.state = {
+            done: false
+        };
+
+        this.onLabelClick = () => {
+            this.setState({
+                done: true
+            })
+        };
+
+    }
+    render() {
+        const {label, important = false} = this.props; // передача свойств - свойства хранятся в this.props
+        const {done} = this.state;
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
       <span
           className="todo-list-item-label"
           style={style}
-          onClick={this.onLabelCkick}  >
+          onClick={this.onLabelClick}>
         {label}
       </span>
 
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
+        <i className="fa fa-exclamation"/>
       </button>
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
+        <i className="fa fa-trash-o"/>
       </button>
     </span>
         );
